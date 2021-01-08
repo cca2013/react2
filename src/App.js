2cf84f6b7546b062d20241beb95ad13122371175
unknown <skafosbiz@gmail.com>
2021-01-08 19:08:26 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+		  
+        </p>
+		<p>Konstantinos P. Anastasiadis</p>
+		<p>Οι γονείς μου είναι ο Παναγιώτης και η Ζωή Αναστασιάδου</p>
+		<p>React - cd mysecondreact</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
