217be24dce55cebb4239fbb707a19c05bed838e8
unknown <skafosbiz@gmail.com>
2021-01-08 20:26:22 +0200

    'mysecondreact'

--- src/App.js
+++ src/App.js
@@ -6,13 +6,27 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-		  
-        </p>
-		<p>Konstantinos P. Anastasiadis</p>
+
+		<p> 
+		<a
+          className="App-link"
+          href="http:\\vresto.eu"
+          target="_blank"
+          rel="noopener noreferrer"
+          >
+		Konstantinos P. Anastasiadis</a> 
+		</p>
 		<p>Οι γονείς μου είναι ο Παναγιώτης(1973) και η Ζωή Αναστασιάδου(2015)</p>
-		<p>React - cd mysecondreact</p>
+		<p>
+		 <a
+          className="App-link"
+          href="https://github/cca2013/react2"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+		React - cd mysecondreact</a>
+		
+		</p>
         <a
           className="App-link"
           href="https://reactjs.org"
